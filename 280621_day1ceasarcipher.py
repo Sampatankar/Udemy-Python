1403a19f5638773a9f3d62e4f79995b2705404f8
Sam Patankar <50807100+Sampatankar@users.noreply.github.com>
2021-06-28 19:44:30 +0100

    Function and method practice to-date with algo work

---
+++ 280621_day1ceasarcipher.py
@@ -0,0 +1,43 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+# for letter in alphabet:
+#   print(alphabet.index(letter))
+
+# How to get index numbers of letters in 'text':
+for letter in text:
+  print(alphabet.index(letter)) # integer
+  
+  # How to get right index no. depending on shift:
+  new_index = alphabet.index(letter) + shift
+  if new_index > 25:
+    new_index = 0 + shift
+  else:
+    new_index = new_index
+
+  # Print out encoded message:
+  
+    
+
+
+
+def encrypt(text, shift):
+  pass  
