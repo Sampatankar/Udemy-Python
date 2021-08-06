4c49e8948916bbe7d3b149636d8bbe48e16871f7
Sam Patankar <50807100+Sampatankar@users.noreply.github.com>
2021-08-06 22:40:22 +0100

    First commit for True/False quiz

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+  {"text": "A slug's blood is green.", "answer": "True"},
+  {"text": "The loudest animal is the African elephant.", "answer": "False"},
+  {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+  {"text": "The total surface area of a humans lungs are the size of a football pitch.", "answer": "True"},
+  {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to eat it.", "answer": "True"},
+  {"text": "In London, U.K., if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+  {"text": "You can lead a cow downstairs but not upstairs.", "answer": "False"},
+  {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+  {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+  {"text": "It is illegal to pee in the ocean in Portugal.", "answer": "True"},
+  {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+  {"text": "A few ounces of chocolate can kill a small dog.", "answer": "True"},
+]
---
+++ quiz_brain.py
@@ -0,0 +1,9 @@
+from main import question_bank
+class QuizBrain:
+  
+  def __init__(self, q_list):
+    self.question_number = 0
+    self.question_list = q_list
+
+  def next_question(self, question_bank):
+    pass
