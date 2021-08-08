68c68ad06730193c11421deb8be5d41b03b28093
Sam Patankar <50807100+Sampatankar@users.noreply.github.com>
2021-08-08 12:09:40 +0100

    Final commit for quiz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -7,5 +8,14 @@ for question in question_data:
   new_question = Question(question["text"], question["answer"])
   question_bank.append(new_question)
 
-# print(question_bank)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+  quiz.next_question()
+
+
+print("You have completed the quiz.")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
+
+# https://opentdb.com/api.php?amount=10
 
